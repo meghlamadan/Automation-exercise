be97ed45296ae6171beeb0e220364812040fb231
meghlamadan <71842950+meghlamadan@users.noreply.github.com>
2024-06-15 16:23:14 +0100

    Add files via upload

---
+++ Automation-Exercise/src/main/java/words/Words.java
@@ -0,0 +1,11 @@
+package words;
+
+import java.util.List;
+
+import org.openqa.selenium.UnsupportedCommandException;
+
+public class Words {
+    public static List<String> getUniqueWordsFromSentence(String sentence) {
+        throw new UnsupportedCommandException();
+    }
+}
